8d64f0f13e3df484a74c47600aa601f476ee1378
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-16 09:11:14 -0300

    feat: create post page and implement all functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi'
+import Head from 'next/head'
 import { getPrismicClient } from '../services/prismic';
 import styles from './home.module.scss';
 import { DefaultLayout } from '../layouts/DefaultLayout';
@@ -58,38 +59,43 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
   };
 
   return (
-    <DefaultLayout>
-      <Header />
-      <div className={styles.posts}>
-        {posts.map(post => (
-          <Link href={`/post/${post.uid}`} key={post.uid}>
-            <section className={styles.card}>
-              <h2>{post.data.title}</h2>
-              <h3>{post.data.subtitle}</h3>
-              <div className={styles.iconWrapper}>
-                <div>
-                  <FiCalendar color="#BBBBBB" size={20} />
-                  <span>{formatDate(post.first_publication_date)}</span>
+    <>
+      <Head>
+        <title>Home | Spacetraveling</title>
+      </Head>
+      <DefaultLayout>
+        <Header />
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <section className={styles.card}>
+                <h2>{post.data.title}</h2>
+                <h3>{post.data.subtitle}</h3>
+                <div className={styles.iconWrapper}>
+                  <div>
+                    <FiCalendar color="#BBBBBB" size={20} />
+                    <span>{formatDate(post.first_publication_date)}</span>
+                  </div>
+                  <div>
+                    <FiUser color="#BBBBBB" size={20} />
+                    <span>{post.data.author}</span>
+                  </div>
                 </div>
-                <div>
-                  <FiUser color="#BBBBBB" size={20} />
-                  <span>{post.data.author}</span>
-                </div>
-              </div>
-            </section>
-          </Link>
-        ))}
-        {showLoadMoreButton && (
-          <button
-            type='button'
-            className={styles.loadMorePosts}
-            onClick={handleLoadMorePosts}
-          >
-            <span>Carregar mais posts</span>
-          </button>
-        )}
-      </div>
-    </DefaultLayout>
+              </section>
+            </Link>
+          ))}
+          {showLoadMoreButton && (
+            <button
+              type='button'
+              className={styles.loadMorePosts}
+              onClick={handleLoadMorePosts}
+            >
+              <span>Carregar mais posts</span>
+            </button>
+          )}
+        </div>
+      </DefaultLayout>
+    </>
   );
 }
 
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,15 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './post.module.scss';
+import { calcReadTime } from '../../utils/calcReadTime';
+import { PostLayout } from '../../layouts/PostLayout';
+import Header from '../../components/Header';
+import { formatDate } from '../../utils/formatDate';
 
 interface Post {
   first_publication_date: string | null;
@@ -23,8 +32,87 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): JSX.Element {
-  return <h1>POST</h1>;
+export default function Post({ post }: PostProps): JSX.Element {
+  const [readTime, setReadTime] = useState(0);
+  const { isFallback } = useRouter();
+
+  const {
+    data: { author, banner, content, title },
+    first_publication_date,
+  } = post!;
+
+  useEffect(() => {
+    if (!isFallback) {
+      const calculatedTime = calcReadTime({ content });
+
+      setReadTime(calculatedTime);
+    }
+  }, []);
+
+  if (isFallback) {
+    return (
+      <div className={styles.loadingContainer}>
+        <h2>Carregando...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{title} | Spacetraveling</title>
+      </Head>
+      <PostLayout>
+        <div className={styles.headerWrapper}>
+          <Header />
+        </div>
+        <div className={styles.bannerWrapper}>
+          <Image src={banner.url} width="auto" height="auto" />
+        </div>
+        <section className={styles.mainContainer}>
+          <section className={styles.heading}>
+            <h1>{title}</h1>
+            <div>
+              <span>
+                <FiCalendar />
+                {formatDate(first_publication_date)}
+              </span>
+              <span>
+                <FiUser />
+                {author}
+              </span>
+              <span>
+                <FiClock />
+                {readTime} min
+              </span>
+            </div>
+          </section>
+          <div className={styles.contentContainer}>
+            {content.map(section => {
+              const sectionKey = JSON.stringify(section);
+
+              return (
+                <section key={sectionKey} className={styles.contentWrapper}>
+                  <h3>{section.heading}</h3>
+                  <div>
+                    {section.body.map((postContent, j) => {
+                      const key = String(j);
+
+                      return (
+                        <div key={key}>
+                          <p>{postContent.text}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </section>
+              );
+            })}
+          </div>
+        </section>
+      </PostLayout>
+    </>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
