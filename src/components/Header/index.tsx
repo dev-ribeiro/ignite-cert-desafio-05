ffc8dca9b280d126800777bda2b584970a15b912
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-16 03:40:02 -0300

    feat: create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <a>
+          <Image
+            src="/assets/logo.png"
+            width={236.82}
+            height={25.63}
+            alt="logo"
+          />
+        </a>
+      </Link>
+    </header>
+  );
 }
